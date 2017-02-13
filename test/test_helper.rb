c1bd67a2d5e6ac1dc2a81008f8b09b415d74759b
Wyatt Kirby <wyatt@apsis.io>
2017-02-13 15:27:18 -0800

    Add coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@ require 'minitest/autorun'
 require 'minitest/mock'
 require 'minitest/reporters'
 require 'mocha/mini_test'
+require 'coveralls'
+
+Coveralls.wear!
 
 Dir['test/fixtures/**/*.rb'].each { |f| require File.expand_path(f) }
 Dir['test/assertions/**/*.rb'].each { |f| require File.expand_path(f) }
