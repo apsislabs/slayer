d7132e1b67a1e739a4e511b50233d6173443222f
Wyatt Kirby <wyatt@apsis.io>
2017-02-13 17:13:18 -0800

    Address rubocop issues

--- lib/slayer/rails_extensions/form.rb
+++ lib/slayer/rails_extensions/form.rb
@@ -1,47 +1,44 @@
-module Slayer
-  module RailsExtensions
-    module Form
-      def self.included(base)
-        base.class_eval do
-          include ActiveModel::Validations
+module Slayer::RailsExtensions
+  module Form
+    def self.included(base)
+      base.extend ClassMethods
+    end
 
-          def validate!
-            validatable = respond_to?(:valid?) && respond_to?(:errors)
+    module ClassMethods
+      include ActiveModel::Validations
 
-            raise NotImplementedError unless validatable
-            raise FormValidationError, errors unless valid?
-          end
+      def validate!
+        validatable = respond_to?(:valid?) && respond_to?(:errors)
 
-          def self.from_params(params, additional_params = {}, root_key: nil)
-            params = params.respond_to?(:to_unsafe_h) ? params.to_unsafe_h : params.to_h
-            params = params.with_indifferent_access
+        raise NotImplementedError unless validatable
+        raise FormValidationError, errors unless valid?
+      end
 
-            attr_names = attribute_set.map(&:name)
+      def self.from_params(params, additional_params = {}, root_key: nil)
+        params     = params.respond_to?(:to_unsafe_h) ? params.to_unsafe_h : params.to_h
+        attr_names = attribute_set.map(&:name)
 
-            attr_hash = params
-              .fetch(root_key, {})
-              .merge(params.slice(*attr_names))
-              .merge(additional_params)
+        attr_hash = params
+                      .with_indifferent_access
+                      .fetch(root_key, {})
+                      .merge(params.slice(*attr_names))
+                      .merge(additional_params)
 
-            new(attr_hash)
-          end
+        new(attr_hash)
+      end
 
-          def self.from_model(model)
-            attr_names = attribute_set.map(&:name)
+      def self.from_model(model)
+        attr_names = attribute_set.map(&:name)
 
-            attr_hash = attr_names.inject({}) do |n, hash|
-              if model.respond_to?(n)
-                hash[n] = model.public_send(n)
-              end
-            end
-
-            new(attr_hash)
-          end
-
-          def self.from_json(json)
-            from_params(JSON.parse(json))
-          end
+        attr_hash = attr_names.inject({}) do |n, hash|
+          hash[n] = model.public_send(n) if model.respond_to?(n)
         end
+
+        new(attr_hash)
+      end
+
+      def self.from_json(json)
+        from_params(JSON.parse(json))
       end
     end
   end
