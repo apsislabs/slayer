6bd2008c6633abb0a519ab15607f7cbf4ff6b1fe
Wyatt Kirby <wyatt@apsis.io>
2017-02-13 22:41:53 -0800

    Bump version

--- lib/slayer/form.rb
+++ lib/slayer/form.rb
@@ -3,13 +3,5 @@ require 'virtus'
 module Slayer
   class Form
     include Virtus.model
-    include ActiveModel::Validations if defined?(Rails)
-
-    def validate!
-      validatable = respond_to?(:valid?) && respond_to?(:errors)
-
-      raise NotImplementedError unless validatable
-      raise FormValidationError, errors unless valid?
-    end
   end
 end
