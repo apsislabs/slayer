6822ab129a27f995ef2595a60c5d679bb47da411
Wyatt Kirby <wyatt@apsis.io>
2017-02-13 16:03:45 -0800

    Fix test coverage metrics

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,25 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter '/test/'
+end
+
 require 'slayer'
 require 'byebug'
 require 'minitest/autorun'
 require 'minitest/mock'
 require 'minitest/reporters'
 require 'mocha/mini_test'
-require 'coveralls'
-
-Coveralls.wear!
-
-Dir['test/fixtures/**/*.rb'].each { |f| require File.expand_path(f) }
-Dir['test/assertions/**/*.rb'].each { |f| require File.expand_path(f) }
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+Dir['test/assertions/**/*.rb'].each { |f| require File.expand_path(f) }
+Dir['test/fixtures/**/*.rb'].each { |f| require File.expand_path(f) }
