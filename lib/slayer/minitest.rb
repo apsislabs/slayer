a5c0f1aafa60ebcbebdc955b0c62fdf7e2f7e48c
Wyatt Kirby <wyatt@apsis.io>
2018-11-16 11:34:06 -0800

    Fix rubocop

--- lib/slayer/minitest.rb
+++ lib/slayer/minitest.rb
@@ -1,23 +1,66 @@
 # :nocov:
 require 'minitest/assertions'
-
+# rubocop:disable Style/Documentation
+# rubocop:disable Metrics/MethodLength
 module Minitest::Assertions
   def assert_success(result, status: nil, message: nil, value: nil)
-    assert result.success?, "Expected command to succeed."
+    assert result.success?, 'Expected command to succeed.'
 
-    assert_equal(status, result.status, "Expected command to succeed with status: :#{status}, got: :#{result.status}") unless status.nil?
-    assert_equal(message, result.message, "Expected command to succeed with message: #{message}, got: #{result.message}") unless message.nil?
-    assert_equal(value, result.value, "Expected command to succeed with value: #{value}, got: #{result.value}") unless value.nil?
+    unless status.nil?
+      assert_equal(
+        status,
+        result.status,
+        "Expected command to succeed with status: :#{status}, got: :#{result.status}"
+      )
+    end
+
+    unless message.nil?
+      assert_equal(
+        message,
+        result.message,
+        "Expected command to succeed with message: #{message}, got: #{result.message}"
+      )
+    end
+
+    unless value.nil?
+      assert_equal(
+        value,
+        result.value,
+        "Expected command to succeed with value: #{value}, got: #{result.value}"
+      )
+    end
   end
   alias refute_failed assert_success
 
   def refute_success(result, status: nil, message: nil, value: nil)
-    refute result.success?, "Expected command to fail."
+    refute result.success?, 'Expected command to fail.'
 
-    refute_equal(status, result.status, "Expected command to fail with status: :#{status}, got: :#{result.status}") unless status.nil?
-    refute_equal(message, result.message, "Expected command to fail with message: #{message}, got: #{result.message}") unless message.nil?
-    refute_equal(value, result.value, "Expected command to fail with value: #{value}, got: #{result.value}") unless value.nil?
+    unless status.nil?
+      refute_equal(
+        status,
+        result.status,
+        "Expected command to fail with status: :#{status}, got: :#{result.status}"
+      )
+    end
+
+    unless message.nil?
+      refute_equal(
+        message,
+        result.message,
+        "Expected command to fail with message: #{message}, got: #{result.message}"
+      )
+    end
+
+    unless value.nil?
+      refute_equal(
+        value,
+        result.value,
+        "Expected command to fail with value: #{value}, got: #{result.value}"
+      )
+    end
   end
   alias assert_failed refute_success
 end
+# rubocop:enable Style/Documentation
+# rubocop:enable Metrics/MethodLength
 # :nocov:
